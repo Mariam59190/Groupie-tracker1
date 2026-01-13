2ae1ec5d3de1ec781a86e37830fa8b9045675e30
Mariam59190 <mariamwalamako@gmail.com>
2026-01-13 08:46:20 +0000

    add Artist model

---
+++ models/artist.go
@@ -0,0 +1,11 @@
+package models
+
+// Artist représente un artiste ou un groupe musical
+type Artist struct {
+	ID           int      `json:"id"`
+	Name         string   `json:"name"`
+	Image        string   `json:"image"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+}
